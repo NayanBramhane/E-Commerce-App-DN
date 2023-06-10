cf1d74fb24cc3d8e8bc9988445ceaeb057ef5d08
Nayan Bramhane <nayan.m.bramhane@gmail.com>
2023-06-11 00:16:35 +0530

    Added payments

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,3 @@
-/* eslint-disable no-unused-vars */
-/* eslint-disable react/jsx-no-undef */
 import "./App.css";
 import { createBrowserRouter } from "react-router-dom";
 import { createRoutesFromElements } from "react-router-dom";
@@ -11,21 +9,47 @@ import Cart from "./pages/Cart";
 import Login from "./pages/Login";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import Checkout from "./pages/Checkout";
+import AuthProvider, { useAuth } from "./firebase/Auth";
+import { Navigate } from "react-router-dom";
+import Register from "./pages/Register";
+
+function ProtectedRoute({ children }) {
+  const { user } = useAuth();
+  if (!user) {
+    return <Navigate to={"/login"} />;
+  }
+  return children;
+}
 
 const router = createBrowserRouter(
   createRoutesFromElements(
-    <Route path="/" element={<Layout />}>
-      <Route index element={<Home />} />
-      <Route path="/cart" index element={<Cart />} />
+    <>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<Home />} />
+        <Route path="/cart" index element={<Cart />} />
+        <Route
+          path="/checkout"
+          index
+          element={
+            <ProtectedRoute>
+              <Checkout />
+            </ProtectedRoute>
+          }
+        />
+      </Route>
       <Route path="/login" index element={<Login />} />
-    </Route>
+      <Route path="/register" index element={<Register />} />
+    </>
   )
 );
 function App() {
   return (
-    <Provider store={store}>
-      <RouterProvider router={router} />;
-    </Provider>
+    <AuthProvider>
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
+    </AuthProvider>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -18,9 +18,11 @@ import { fetchAllCategories } from "../feature/categories-slice";
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { useEffect } from "react";
-import { useSearchParams } from "react-router-dom";
+import { useSearchParams, Link } from "react-router-dom";
 import { useTheme } from "@emotion/react";
 import SearchIcon from "@mui/icons-material/Search";
+import { useAuth } from "../firebase/Auth";
+import { Menu } from "@mui/material";
 const Search = styled("section")(({ theme }) => ({
   position: "relative",
   borderRadius: theme.shape.borderRadius,
@@ -62,6 +64,11 @@ const SearchIconWrapper = styled("section")(({ theme }) => ({
   justifyContent: "center",
 }));
 
+const StyledLink = styled(Link)(({ theme }) => ({
+  color: theme.palette.common.white,
+  textDecoration: "none",
+}));
+
 function SearchBar() {
   const theme = useTheme();
   const products = useSelector((state) => state.products?.value);
@@ -84,11 +91,7 @@ function SearchBar() {
   function handleCategoryChange(event) {
     const { value } = event.target;
     console.log(searchTerm);
-    navigate(
-      value === "all"
-        ? "/"
-        : `/?category=${value}${searchTerm ? "&seachterm=" + searchTerm : ""}`
-    );
+    navigate(value === "all" ? "/" : `/?category=${value}${searchTerm ? "&seachterm=" + searchTerm : ""}`);
   }
   function handleSearchChange(searchText) {
     if (searchText) {
@@ -98,9 +101,7 @@ function SearchBar() {
           : `/?category=${selectedCategory}&searchterm=${searchText}`
       );
     } else {
-      navigate(
-        selectedCategory === "all" ? `/` : `/?category=${selectedCategory}`
-      );
+      navigate(selectedCategory === "all" ? `/` : `/?category=${selectedCategory}`);
     }
   }
   return (
@@ -163,9 +164,7 @@ function SearchBar() {
         }}
         disablePortal
         options={Array.from(
-          selectedCategory === "all"
-            ? products
-            : products.filter((prod) => prod.category === selectedCategory),
+          selectedCategory === "all" ? products : products.filter((prod) => prod.category === selectedCategory),
           (prod) => ({ id: prod.id, label: prod.title })
         )}
         renderInput={(params) => <TextField {...params} />}
@@ -178,28 +177,76 @@ function SearchBar() {
 }
 
 export default function Header() {
+  const { user, signOut } = useAuth();
   const cartItems = useSelector((state) => state.cart?.value);
   const count = getItemCount(cartItems);
+  const navigate = useNavigate();
+  const [anchorEl, setAnchorEl] = useState(null);
+  const isMenuOpen = Boolean(anchorEl);
+  function navigateToCart() {
+    navigate("/cart");
+  }
+  function handleProfileMenuOpen(e) {
+    setAnchorEl(e.currentTarget);
+  }
+  function handleMenuClose() {
+    setAnchorEl(null);
+  }
+  async function logout() {
+    await signOut();
+    navigate("/login");
+  }
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      id="user-profile-menu"
+      keepMounted
+      transformOrigin={{
+        horizontal: "right",
+        vertical: "top",
+      }}
+      anchorOrigin={{
+        horizontal: "right",
+        vertical: "bottom",
+      }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Profile</MenuItem>
+      <MenuItem onClick={handleMenuClose}>My Account</MenuItem>
+      <MenuItem onClick={logout}>Logout</MenuItem>
+    </Menu>
+  );
   return (
-    <AppBar position="sticky">
-      <Toolbar>
-        <Typography variant="h6" color="inherit">
-          Ecomm
-        </Typography>
-        <SearchBar />
-        <Box sx={{ display: { md: "flex" } }}>
-          <IconButton
-            size="large"
-            aria-label="shows cart items count"
-            color="inherit"
-          >
-            <Badge badgeContent={count} color="error">
-              <ShoppingCartIcon />
-            </Badge>
-          </IconButton>
-        </Box>
-        <Button color="inherit">Login</Button>
-      </Toolbar>
-    </AppBar>
+    <>
+      <AppBar
+        position="sticky"
+        sx={{
+          py: 1,
+        }}
+      >
+        <Toolbar sx={{ display: "flex", gap: 2 }}>
+          <Typography variant="h6" color="inherit">
+            <StyledLink to="/">Ecomm</StyledLink>
+          </Typography>
+          <SearchBar />
+          <Box flexBasis={500} sx={{ display: { md: "flex" } }}>
+            <IconButton onClick={navigateToCart} size="large" aria-label="shows cart items count" color="inherit">
+              <Badge badgeContent={count} color="error">
+                <ShoppingCartIcon />
+              </Badge>
+            </IconButton>
+            {user ? (
+              <Button onClick={handleProfileMenuOpen} color="inherit">
+                Hello, {user.displayName ?? user.email}
+              </Button>
+            ) : (
+              <Button color="inherit">Login</Button>
+            )}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      {renderMenu}
+    </>
   );
 }
